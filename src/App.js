d9dc7154c2aa070371a8297234db52168d8090df
Shamaun-Nabi <snashik950@gmail.com>
2021-10-11 17:01:03 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Card from "./Components/Card/Card";
+
+function App() {
+
+  return (
+    <div className="mainBody">
+      <h3 className="text-center mt-4">Movie Server</h3>
+      <Card></Card>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Movies.js
@@ -0,0 +1,63 @@
+const movieList = [
+  {
+    id: 1,
+    image:
+      "https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABY1SO9JhQQ12Hmoj-RAfWRe3iQv4VX-EO3eBh0HQW_Nn3Sv1QVL-cQY0TS6cowvka1JaF0nCSrpC-aPOalpQsew-uzY.jpg?r=24f",
+    name: "Mimi",
+    desc: "An aspiring actress in a small town agrees to bear a child for a visiting couple seeking a surrogate mother, but her experience takes unexpected turns.",
+    Starring: "Kriti Sanon, Pankaj Tripathi, Sai Tamhankar",
+    genre: "Comedies",
+    duration: "2h 12m",
+  },
+  {
+    id: 2,
+    image:
+      "https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYuGzLe962ogE9Ek4mE3-WphgWZgPyNAwC8H-6SeET-zQc5GlOgo9_L_YbSCbOLq54B56fvjBE5CF6XN8EJ5PyS-oNupK0gE9O8xEcjUpGOKTEc9PWG2jp7pZ6LB.jpg?r=f8a",
+    name: "Haseen Dillruba",
+    desc: "Under investigation as a suspect in her husband’s murder, a wife reveals details of their thorny marriage that seem to only further blur the truth.",
+    Starring: "Taapsee Pannu, Vikrant Massey, Harshvardhan",
+    genre: "Romantic Movies",
+    duration: "2h 15m",
+  },
+  {
+    id: 3,
+    image:
+      "https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQKtD_r5rtoOav3NGYAQg0KGA6VHPMjEBz0jbSbruR3sqoAooGlTRjJ2gUeyBp_urlp3wGL-TAQxYAoyU4npXvUjxYVN9h73yArmReZelInAcpXJp1cH2itwe_pL.jpg?r=023",
+    name: "Jagame Thandhiram",
+    desc: "When a clever, carefree gangster is recruited to help an overseas crime lord take down a rival, he is caught off guard by the moral dilemmas that follow",
+    Starring: "Dhanush, Aishwarya Lekshmi, James Cosmo",
+    genre: "Action",
+    duration: "2h 38m",
+  },
+  {
+    id: 4,
+    image:
+      "https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdGY4Y0Ij2LTz1jANkNN1C5yT8Sg0ukAw3AHXXY6eRhmxufub0Na2EJwvoCPoluWl6kO5dLgmO-c_uBngSsVy4KOjbU.jpg?r=81c",
+    name: "Saaho",
+    desc: "In a fictional megalopolis, a stunning underworld theft triggers a fierce power struggle among the gangsters, with an enigmatic cop on their trail",
+    Starring: "Prabhas, Shraddha Kapoor, Jackie Shroff",
+    genre: "Bollywood Movies",
+    duration: "2h 51m",
+  },
+  {
+    id: 5,
+    image:
+      "https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdGY4Y0Ij2LTz1jANkNN1C5yT8Sg0ukAw3AHXXY6eRhmxufub0Na2EJwvoCPoluWl6kO5dLgmO-c_uBngSsVy4KOjbU.jpg?r=81c",
+    name: "Saaho",
+    desc: "In a fictional megalopolis, a stunning underworld theft triggers a fierce power struggle among the gangsters, with an enigmatic cop on their trail",
+    Starring: "Prabhas, Shraddha Kapoor, Jackie Shroff",
+    genre: "Bollywood Movies",
+    duration: "2h 51m",
+  },
+  {
+    id: 6,
+    image:
+      "https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdGY4Y0Ij2LTz1jANkNN1C5yT8Sg0ukAw3AHXXY6eRhmxufub0Na2EJwvoCPoluWl6kO5dLgmO-c_uBngSsVy4KOjbU.jpg?r=81c",
+    name: "Saaho",
+    desc: "In a fictional megalopolis, a stunning underworld theft triggers a fierce power struggle among the gangsters, with an enigmatic cop on their trail",
+    Starring: "Prabhas, Shraddha Kapoor, Jackie Shroff",
+    genre: "Bollywood Movies",
+    duration: "2h 51m",
+  },
+];
+export default movieList
